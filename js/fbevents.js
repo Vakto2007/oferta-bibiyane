46d91b48b474ee15fd747ce332987665fc66878b
Vakto2007 <caminhonaturalmz@gmail.com>
2026-05-28 17:07:07 +0200

    Add files via upload

--- js/fbevents.js
+++ js/fbevents.js
@@ -17,7 +17,7 @@
 * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-fbq.version="2.9.326";
+fbq.version="2.9.327";
 fbq._releaseSegment = "stable";
 fbq.pendingConfigs=["global_config"];
 fbq.__openBridgeRollout = 1.0;
